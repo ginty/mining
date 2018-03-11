ee212748a7e7b4d531d8e5e02226bcf0972f4682
Stephen McGinty <stephen.f.mcginty@gmail.com>
2018-03-11 23:20:58 +0000

    Added monitor

--- fan.sh
+++ fan.sh
@@ -1,4 +1,4 @@
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=80
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=80
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=70
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=70
 sudo nvidia-smi -pm 1
-sudo nvidia-smi -pl 280
+sudo nvidia-smi -pl 180
---
+++ monitor
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+watch -n 0.5 nvidia-smi
